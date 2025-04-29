7093f8107bb70138189394c9e509159b1e6beb8e
Roland Metivier <metivier.roland@chlorophyt.us>
2025-04-29 18:16:56 -0500

    checkdep should check .pkg.tar.zst

--- checkdep.sh
+++ checkdep.sh
@@ -5,7 +5,7 @@ CACHEDIR="/srv/archbyte"
 for candidate in $@; do
   PACKAGE="$candidate-$(pacman -Si $candidate | grep Version | awk '{print $3}')"
 
-  if [ -f "$CACHEDIR/$PACKAGE.tar.zst" ]; then
+  if [ -f "$CACHEDIR/$PACKAGE.pkg.tar.zst" ]; then
     echo "(ARCHBYTE) -> Cache hit, not downloading: $PACKAGE"
   else
     echo "(ARCHBYTE) -> Cache miss, downloading: $PACKAGE"
