6020a44acf1c574bff8e62816a13c80a073875cb
Roland Metivier <metivier.roland@chlorophyt.us>
2025-04-29 18:28:35 -0500

    checkdep should check for any

--- checkdep.sh
+++ checkdep.sh
@@ -6,7 +6,7 @@ ARCHITECTURE="x86_64"
 for candidate in $@; do
   PACKAGE="$candidate-$(pacman -Si $candidate | grep Version | awk '{print $3}')"
 
-  if [ -f "$CACHEDIR/$PACKAGE-$ARCHITECTURE.pkg.tar.zst" ]; then
+  if [ -f "$CACHEDIR/$PACKAGE-$ARCHITECTURE.pkg.tar.zst" ] || [ -f "$CACHEDIR/$PACKAGE-any.pkg.tar.zst" ]; then
     echo "(ARCHBYTE) -> Cache hit, not downloading: $PACKAGE"
   else
     echo "(ARCHBYTE) -> Cache miss, downloading: $PACKAGE"
