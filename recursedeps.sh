ea1080793e9bc5412ebaf236803107a79f3065a6
Roland Metivier <metivier.roland@chlorophyt.us>
2025-05-01 09:14:46 -0500

    try different FDs 2

--- recursedeps.sh
+++ recursedeps.sh
@@ -5,7 +5,7 @@ CACHEDIR="/srv/archbyte"
 ARCHITECTURE="x86_64"
 
 check_dependency () {
-  PACKAGE="$candidate-$(pacman -Si $1 | grep Version | awk '{print $3}')"
+  PACKAGE="$1-$(pacman -Si $1 | grep Version | awk '{print $3}')"
   if [ -f "$CACHEDIR/$PACKAGE-$ARCHITECTURE.pkg.tar.zst" ] || [ -f "$CACHEDIR/$PACKAGE-any.pkg.tar.zst" ]; then
     echo "(ARCHBYTE) -> Cache hit, not downloading: $PACKAGE"
   else
@@ -15,13 +15,13 @@ check_dependency () {
 }
 
 get_dependencies () {
-  while read -u 4 DEPENDENCY; do 
-    echo "(ARCHBYTE) -> Detected dependency $DEPENDENCY in $1"
-    check_dependency $DEPENDENCY
+  while read -r -u 4 dependency; do 
+    echo "(ARCHBYTE) -> Detected dependency $dependency in $1"
+    check_dependency $dependency
   done 4< <(pactree -u $1 | grep "^[^<=>]*$")
 }
 
-while read -u 3 PACKAGE; do
-  echo "(ARCHBYTE) -> Getting dependencies for package: $PACKAGE"
-  get_dependencies $PACKAGE
+while read -r -u 3 line; do
+  echo "(ARCHBYTE) -> Getting dependencies for package: $line"
+  get_dependencies $line
 done 3< $PACKAGELIST
