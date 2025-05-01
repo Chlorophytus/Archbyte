77e97ae07b0c495939d8e57090321134ea5d6c1c
Roland Metivier <metivier.roland@chlorophyt.us>
2025-05-01 08:40:48 -0500

    ok, recursedeps should actually iterate

--- recursedeps.sh
+++ recursedeps.sh
@@ -4,6 +4,8 @@ PACKAGELIST="/srv/archbyte/packages.txt"
 
 while read PACKAGE; do
   echo "(ARCHBYTE) -> Getting dependencies for package: $PACKAGE"
-  ALLDEPS="$(pactree -u $PACKAGE | grep "^[^<=>]*$" | tr '\n' ' ')"
-  /usr/local/bin/checkdep.sh $ALLDEPS
+  while read DEPENDENCY; do 
+    echo "(ARCHBYTE) -> Detected dependency $DEPENDENCY in $PACKAGE"
+    /usr/local/bin/checkdep.sh $DEPENDENCY
+  done < "$(pactree -u $PACKAGE | grep "^[^<=>]*$")"
 done < $PACKAGELIST
